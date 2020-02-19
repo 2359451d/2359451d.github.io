e0215fdec760b9721fba2d7b9d2097cfbbbf5aa1
2359451d <2359451d@student.gla.ac.uk>
2020-02-19 02:24:52 +0000

    scrollspy

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,142 +1,33 @@
-var BlogDirectory = {
+// wrap markdown-toc in navbar div
+$('#markdown-toc').wrapAll('<nav id="post-nav" class="navbar post-nav">');
 
-    /*
-     * Side Nav Affixing
-     *
-     * reference to:
-     * https://github.com/twbs/bootstrap/blob/master/docs/assets/js/src/application.js#L35
-     */
-    setSideNavAffixing:function() {
+// add scrollspy to post navigation
+$('body').scrollspy({target: "#post-nav", offset: 50});
 
-        // Scrollspy
-        var $window = $(window)
-        var $body   = $(document.body)
+// format toc objects with classes for custom scrollspy
+$('#markdown-toc').addClass('nav justify-content-center');
+$('#markdown-toc li').addClass('nav-item');
+$('#markdown-toc a').addClass('nav-link');
 
-        $body.scrollspy({
-            target: '.bs-docs-sidebar'
-        })
-        $window.on('load', function () {
-            $body.scrollspy('refresh')
-        })
+// capitalize headings - because I do not in markdown
+$('#markdown-toc a').css({"text-transform": "capitalize"});
 
-        // Kill links
-        $('.bs-docs-container [href=#]').click(function (e) {
-            e.preventDefault()
-        })
+// loop thru toc list, get headings
+// pad each heading to clear post-nav when linked
+$('#markdown-toc li a').each(function(index) {
+  heading = $(this).attr('id');
+  // remove auto pre text from toc id
+  heading = heading.replace("markdown-toc-", "")
+  $("#"+heading).css({"padding-top": "50px"});
+});
 
-        // Sidenav affixing
-        setTimeout(function () {
-            var $sideBar = $('.bs-docs-sidebar')
-
-            $sideBar.affix({
-                offset: {
-                    top: function () {
-                        var offsetTop      = $sideBar.offset().top
-                        var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-                        var navOuterHeight = $('.bs-docs-bar-top').height()
-
-                        return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-                    },
-                    bottom: function () {
-                        return (this.bottom = 0 - $('.bs-docs-container').outerHeight(true))
-                    }
-                }
-            })
-        }, 100)
-
-        setTimeout(function () {
-            $('.bs-top').affix()
-        }, 100)
-    }, // end of setSideNavAffixing:function()
-
-
-    /* Generate directory tree
-     *
-     * side_nav_element: side navigation element
-     * article_body_element:  article body container.
-     *
-     * processing: search header elements(h1,h2,h3) in `article_body_element`,
-     * generate directory tree list, and put them into side_nav_element.
-     */
-    createBlogDirectory:function (side_nav_element, article_body_element){
-        if(!article_body_element || article_body_element.length < 1 ||
-                !side_nav_element) {
-            return false;
-        }
-
-        var nodes = article_body_element.find("h1,h2,h3");
-        var ulSideNav = side_nav_element;
-
-        $.each(nodes,function(){
-            var $this = $(this);
-
-            var nodetext = $this.text();
-            // There maybe HTML tags in header inner text, use regex to erase them
-            nodetext = nodetext.replace(/<\/?[^>]+>/g,"");
-            nodetext = nodetext.replace(/&nbsp;/ig, "");
-
-            // btw: Jekyll generates id for each header.
-            var nodeid = $this.attr("id");
-            if(!nodeid) {
-                nodeid = "top";
-            }
-
-            var item_a = $("<a></a>");
-            item_a.attr("href", "#" + nodeid);
-            item_a.text(nodetext);
-
-            var ret_li;
-            // wrapper: ul ( in the template, outside this code )
-            // h1: layer 1: li - a
-            // h2: layer 2: ul - li - a
-            // h3: layer 3: ul - ul - li - a
-            switch($this.get(0).tagName) {
-            case "H1":
-                var li_a = $("<li></li>").append(item_a);
-                ret_li = li_a;
-                break;
-            case "H2":
-                var li_a = $("<li></li>").append(item_a);
-                var nav_li_a = $("<ul class=\"nav\"></ul>").append(li_a);
-                ret_li = nav_li_a;
-                break;
-            case "H3":
-                var li_a = $("<li></li>").append(item_a);
-                var nav_li_a = $("<ul class=\"nav\"></ul>").append(li_a);
-                var nav_nav_li_a = $("<ul class=\"nav\"></ul>").append(nav_li_a);
-                ret_li = nav_nav_li_a;
-                break;
-            }
-
-            if(!ret_li) {
-                // do nothing
-            } else {
-                ulSideNav.append(ret_li);
-            }
-        })  // end of each
-
-    } //end of createBlogDirectory:function()
-
-};
-
-
-jQuery(function($) {
-    $(document).ready( function() {
-        /* qrcode
-        var opt = { text : window.location.href, width:150, height:150 };
-        try {
-            document.createElement("canvas").getContext("2d");
-        } catch (e) {
-            $.extend(opt,{ render : "table" });
-        }
-        $('.qrcode').html('').qrcode(opt);
-    */
-
-        // Generate the side navigation `ul` elements
-//        BlogDirectory.createBlogDirectory($("#sideNav"), $(".bs-docs-container"));
-        BlogDirectory.createBlogDirectory($("#sideNav"), $(".post-container"));
-
-        // caculate affixing
-        BlogDirectory.setSideNavAffixing();
-    });
+// fix post navbar on scroll past
+var distanceFromTop = $('#post-nav').offset().top;
+$(window).scroll(function() {
+  var currentScroll = $(window).scrollTop();
+  if (currentScroll >= distanceFromTop) {
+    $('#post-nav').addClass('fixed-top');
+  } else {
+    $('#post-nav').removeClass('fixed-top');
+  }
 });
